ef76d9f1bfc6ad2b2246bff1f791350f316d050c
Woody Hu <whu@marinsoftware.com>
2025-02-11 18:10:41 +0800

    update code and documents

--- kuakuaBot/plugins/ask_deepseek/__init__.py
+++ kuakuaBot/plugins/ask_deepseek/__init__.py
@@ -7,10 +7,11 @@ from nonebot.adapters import Message
 from nonebot.params import CommandArg
 
 
-from nonebot.adapters.onebot.v11 import MessageEvent
+# from nonebot.adapters.onebot.v11 import MessageEvent
+from nonebot.adapters.onebot.v11 import PrivateMessageEvent, GroupMessageEvent
 
 from .config import Config
-from .prompt import send_message
+from .prompt import send_message_v3, send_message_r1
 from .utils import whitelist_groups
 
 __plugin_meta__ = PluginMetadata(
@@ -25,9 +26,24 @@ config = get_plugin_config(Config)
 ask = on_message(rule=to_me(), priority=10)
 
 @ask.handle()
-async def handle_function(event: MessageEvent):
+async def handle_function(event: GroupMessageEvent):
     if question := event.get_message():
         logger.info(question)
-        answer = await send_message(question)
+        answer = await send_message_v3(question)
         logger.info(answer)
+        await ask.finish(answer)
+
+@ask.handle()
+async def handle_function(event: PrivateMessageEvent):
+    if question := event.get_message():
+        logger.info(question)
+        await ask.send("收到问题，正在问 deepseek")
+#         answer, resason = await send_message_r1(question)
+#         if resason is None:
+#             await ask.finish(f"请求出错，请重试。错误信息为: {answer}")
+#         await ask.send(f"""以下是思考过程：
+# {resason}""")
+
+        answer = await send_message_v3(question)
+        
         await ask.finish(answer)
--- kuakuaBot/plugins/ask_deepseek/prompt.py
+++ kuakuaBot/plugins/ask_deepseek/prompt.py
@@ -9,21 +9,17 @@ with open('./plugins/ask_deepseek/.deepseek_key', 'r') as f:
     api_key = f.read()
 
 
-async def send_message(message):
+async def send_message_v3(message):
     payload = {
         "model": "deepseek-ai/DeepSeek-V3",
         "messages": [
             {
                 "role": "user",
-                "content": f"""请问：
-{message}
-
-请返回纯文本答案并且总结到 100 字以内
-"""
+                "content": f"""{message}"""
             }
         ],
         "stream": False,
-        "max_tokens": 100,
+        "max_tokens": 4096,
         "stop": ["null"],
         "temperature": 0.7,
         "top_p": 0.7,
@@ -52,4 +48,49 @@ async def send_message(message):
     logger.info(type(response.text))
     logger.info(response.text)
     responseObj = json.loads(response.text)
+    if "code" in responseObj:
+        return responseObj['message']
     return responseObj['choices'][0]['message']['content']
+
+async def send_message_r1(message):
+    payload = {
+        "model": "deepseek-ai/DeepSeek-R1",
+        "messages": [
+            {
+                "role": "user",
+                "content": f"{message}"
+            }
+        ],
+        "stream": False,
+        "max_tokens": 8192,
+        "stop": ["null"],
+        "temperature": 0.7,
+        "top_p": 0.7,
+        "top_k": 50,
+        "frequency_penalty": 0,
+        "n": 1,
+        "response_format": {"type": "text"},
+        # "tools": [
+        #     {
+        #         "type": "function",
+        #         "function": {
+        #             "description": "<string>",
+        #             "name": "<string>",
+        #             "parameters": {},
+        #             "strict": False
+        #         }
+        #     }
+        # ]
+    }
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    async with httpx.AsyncClient(timeout=500) as client:
+        response = await client.request("POST", url, json=payload, headers=headers)
+    logger.info(type(response.text))
+    logger.info(response.text)
+    responseObj = json.loads(response.text)
+    if "code" in responseObj:
+        return responseObj['message'], None
+    return (responseObj['choices'][0]['message']['content'], responseObj['choices'][0]['message']['reasoning_content'])
