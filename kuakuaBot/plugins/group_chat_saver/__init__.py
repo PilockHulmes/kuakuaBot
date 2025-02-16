afa5573fd877570b1939356b231d915428772f31
Woody Hu <whu@marinsoftware.com>
2025-02-16 10:53:34 +0800

    init group chat saver plugin

---
+++ kuakuaBot/plugins/group_chat_saver/__init__.py
@@ -0,0 +1,21 @@
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="group_chat_saver",
+    description="",
+    usage="",
+    config=Config,
+)
+
+config = get_plugin_config(Config)
+
+whiltelist = [853041949, 1020882307]
+async def group_in_whitelist(event: Event):
+    if (session := event.get_session_id()).startswith("group_"):
+        group_id = session.split("_")[1]
+        return int(group_id) in whiltelist
+    return False
+
