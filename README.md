d269ea718513ad054c8a816ac23ef59943c56efa
woody <user@userdeMacBook-Air.local>
2025-05-05 11:27:21 +0800

    refine

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ This project uses nonebot2 and python 3.9.6, please refer to https://nonebot.dev
 2. 参考 https://github.com/LiteLoaderQQNT/LiteLoaderQQNT 给 qq 安装 qqnt。可以去 https://github.com/Mzdyl/LiteLoaderQQNT_Install/releases 里找最新的一键安装方式
 3. 参考 https://github.com/LLOneBot/LLOneBot 安装 onebot 插件
 4. 参考 https://github.com/nonebot/nonebot2 初始化环境。一般来说就是选 python 3.12 开一个虚拟环境，安装必要库最后用 `nb init` 初始化（初始化的时候随便初始化一个异名插件就可以，主要需要初始化过程中安装的各种东西）
-5. 在 llonebot 里建立反向ws，地址用 `ws://127.0.0.1:8080/onebot/v11/ws`。正向的端口用 6700 即可。
+5. 在 llonebot 里建立反向ws，地址用 `ws://127.0.0.1:8080/onebot/v11/ws`。正向的端口用 `6700` 即可。
---
+++ kuakuaBot/plugins/hachimi_ha/__init__.py
@@ -0,0 +1,80 @@
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+from nonebot import logger, on_command, on_regex, require, get_bot, on_message
+from nonebot.internal.adapter import Bot
+from nonebot.rule import Rule
+from nonebot.adapters import Message
+from nonebot.params import CommandArg
+from nonebot.adapters.onebot.v11 import PrivateMessageEvent, GroupMessageEvent, Event, MessageSegment
+import httpx
+import json
+from datetime import datetime
+import time
+import random
+from pathlib import Path
+
+from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    name="hachimi_ha",
+    description="",
+    usage="",
+    config=Config,
+)
+
+config = get_plugin_config(Config)
+
+group_whitelist = [
+    244960293,
+    # 1020882307,
+]
+def group_in_whitelist(event: GroupMessageEvent):
+    return event.group_id in group_whitelist
+
+qq_whitelist = [
+    506604898, 
+    392206976,
+]
+def qq_in_whitelist(event: GroupMessageEvent):
+    return event.user_id in qq_whitelist
+
+HA_INTERVAL = 600
+group_cooldown = {}
+def on_cooldown(event: GroupMessageEvent):
+    if event.group_id in group_cooldown:
+        print(group_cooldown[event.group_id], time.time())
+    if event.group_id not in group_cooldown:
+        group_cooldown[event.group_id] = time.time()
+        return True
+    if group_cooldown[event.group_id] + HA_INTERVAL > time.time():
+        logger.info("ha on cooldown")
+        return False
+    else:
+        group_cooldown[event.group_id] = time.time()
+        return True
+
+HA_RATE = 0.006
+def should_ha():
+    rand_num = random.random()
+    logger.info(f"get rand_num {rand_num} and rate {HA_RATE}")
+    return rand_num < HA_RATE
+
+ha = on_message(rule= Rule(group_in_whitelist) & Rule(should_ha), priority=5, block=True) # 哈气的时候拒绝其他服务
+
+HA_IMAGES_FOLDER = Path("./plugins/hachimi_ha/assets")
+SUPPORTED_EXTENSIONS = {".jpg", ".jpeg", ".png", ".gif"}
+@ha.handle()
+async def _(event: GroupMessageEvent):
+    # if not on_cooldown(event):
+    #     await ha.finish()
+    image_files = [f for f in HA_IMAGES_FOLDER.iterdir() if f.suffix.lower() in SUPPORTED_EXTENSIONS]
+    if not image_files:
+        await ha.finish()
+    
+    # 随机选择一张图片
+    selected_image = random.choice(image_files)
+       # 构建图片消息
+    image_msg = MessageSegment.image(selected_image)
+    
+    # 发送图片
+    await ha.finish(image_msg)
